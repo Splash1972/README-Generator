0ee310c2555631e83aa44554b96dbdee55de1b6a
Colin Robinson <colindrums@me.com>
2024-05-11 13:26:37 -0400

    initial commit

---
+++ Develop/index.js
@@ -0,0 +1,88 @@
+// TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown.js");
+const path = require("path");
+
+// inquirer
+//   .prompt([
+//     {
+//       type: "input",
+//       message: "What will the title be for your README file?",
+//       name: "readMeTitle",
+//     }
+//   ])
+//   .then((response) =>
+//     console.log(`Your response was ${response.readMeTitle}`)
+//   );
+
+
+// TODO: Create an array of questions for user input
+
+const questions = [
+  {
+    type: "input",
+    name: "title",
+    message: "Enter the title of your project.",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Please describe the project.",
+  },
+  {
+    type: "input",
+    name: "link",
+    message: "Please provide a URL where a user can access your deployed application."
+  },
+  {
+    type: "checkbox",
+    name: "license",
+    message: "Please select a license applicable to this project.",
+    choices: ["MIT", "APACHE2.0", "Boost1.0", "MPL2.0", "BSD2", "BSD3", "none"],
+  },
+  {
+    type: "input",
+    name: "features",
+    message: "Unique features this project has in it.",
+  },
+  {
+    type: "input",
+    name: "language",
+    message: "State the languages or dependencies used in this project.",
+  },
+  {
+    type: "input",
+    name: "contributors",
+    message: "List the contributors on this project.",
+  },
+  {
+    type: "input",
+    name: "creator",
+    message: "Write your GitHub username.",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "Provide a valid email address.",
+  },
+];
+
+// TODO: Create a function to write README file
+
+const writeToFile = (data) => {
+    fs.writeFile('README.md', data, (err) => {
+        err ? console.error(err) : console.log('added to README.md')
+    })
+}
+
+
+// TODO: Create a function to initialize app
+function init() {
+  inquirer.prompt(questions).then((responses) => {
+    console.log("Creating README.md file...");
+    writeToFile("./README.md", generateMarkdown({ ...responses}));
+  });
+}
+// Function call to initialize app
+init();
---
+++ Develop/utils/generateMarkdown.js
@@ -0,0 +1,55 @@
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+function renderLicenseBadge(license) {
+  if (license !== "none") {
+    return `![Github license](https://img.shields.io/badge/license-${license}-blue.svg)`;
+  }
+  return "";
+}
+
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+function renderLicenseLink(license) {
+  if (license !== "none") {
+    return `https://www.npmjs.com/search?q=${license}`;
+  }
+  return "";
+}
+
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) {
+  if (typeof license !== 'string' || license === 'none') {
+    return '';
+  }
+  return `/README.md#${license}`;
+}
+
+// TODO: Create a function to generate markdown for README
+function generateMarkdown(data) {
+  return `# ${data.title}
+  ${renderLicenseBadge(data.license)}
+  ${renderLicenseLink(data.license)}
+## Description
+${data.description}
+## Deployed Application URL
+${data.link}
+## Table of Contents
+* [Features](#features)
+* [Languages & Dependencies](#languagesanddependencies)
+* [How to Use This Application](#HowtoUseThisApplication)
+* [Contributors](#contributors)
+* [Testing](#testing)
+* [Questions](#questions)
+## Features
+${data.features}
+## Languages & Dependencies
+${data.language}
+## Contributors
+${data.contributors}
+## Questions
+Please send your questions [here](mailto:${data.email}?subject=[GitHub]%20Dev%20Connect) or visit [github/${data.creator}](https://github.com/${data.creator}).
+`;
+}
+
+module.exports = generateMarkdown;
